c1fa9a8d0ba708236c995d459d685727d098e74a
Caio Martins <caiomartinst@gmail.com>
2022-03-31 10:38:17 -0300

    version-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import {ModuleWithProviders} from '@angular/core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {ProdutosComponent} from './produtos/produtos.component';
+import {ContatoComponent} from './contato/contato.component';
+
+const routes: Routes = [
+  {path: 'produtos', component: ProdutosComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
